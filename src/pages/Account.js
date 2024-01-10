86f4dcec1140c4cb9266c5f105b3bf5afaa10c83
Onah Bernard Chizoba <onahbernardchizoba@gmail.com>
2024-01-10 12:53:59 +0100

    general ammendments

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,15 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
-import useAxiosPrivate from '../hooks/useAxiosPrivate';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './Account.scss';
 import { faArrowRightToBracket, faUserCircle, faUserEdit } from '@fortawesome/free-solid-svg-icons';
 
 const Account = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
+  const name = localStorage.getItem('name');
+  const email = localStorage.getItem('email');
 
-  const axiosPrivate = useAxiosPrivate();
+  const firstName = name.split(' ')[0];
+
 
   const logout = () => {
     let keysToRemove = ["token", "email", "hash", "firstName", "name", "logoutName"];
@@ -21,28 +21,6 @@ const Account = () => {
     window.location.href = '/';
   }
 
-  useEffect(() => {
-    const getUser = async () => {
-      try {
-        const response = await axiosPrivate.get('/user', {
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: 'Bearer ' + localStorage.getItem('token'),
-          },
-          withCredentials: true
-        });
-
-        setEmail(response.data.user.email);
-        setName(response.data.user.firstName);
-
-      } catch (err) {
-        console.log(err);
-      }
-    }
-
-    getUser();
-  }, [axiosPrivate]);
-
   return (
     <section className="acc-s">
       <div className="acc">
@@ -50,7 +28,7 @@ const Account = () => {
           <article className="card">
             <ul className="list">
               <div className="basic-info">
-                <h6>{name}</h6>
+                <h6>{firstName}</h6>
                 <p>{email}</p>
               </div>
               <li>
